b3c79067b0b902c196e9509649b8c5ef4851b7a6
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-17 19:45:03 +0300

    added wallet icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
+
 import './navbar.css';
 
 export default function Navbar() {
     return (
         <nav>
             <h1>Mirror Copy</h1>
-            <button>Connect</button>
+            <button>
+                <Image src='/icons/wallet.svg' alt='wallet' height={32} width={32} />
+                Connect
+            </button>
         </nav>
     );
 }

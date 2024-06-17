786488e1074c637638357d3d6ae6af603103eb0a
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-17 21:58:55 +0300

    added petra wallet connection

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,35 @@
+'use client';
+
 import Image from 'next/image';
+import { useMemo } from 'react';
+
+import { useWallet } from '@aptos-labs/wallet-adapter-react';
+import { PetraWalletName } from 'petra-plugin-wallet-adapter';
 
 import './navbar.css';
 
 export default function Navbar() {
+    const { connect, account } = useWallet();
+
+    const buttonMessage = useMemo(
+        () =>
+            account
+                ? account.address.slice(0, 4) + '...' + account.address.slice(60, 64)
+                : 'Connect',
+        [account]
+    );
+
     return (
         <nav>
             <h1>Mirror Copy</h1>
-            <button>
+            <button
+                onClick={() => {
+                    if ('aptos' in window) connect(PetraWalletName);
+                    else window.open('https://petra.app/', `_blank`);
+                }}
+            >
                 <Image src='/icons/wallet.svg' alt='wallet' height={32} width={32} />
-                Connect
+                {buttonMessage}
             </button>
         </nav>
     );

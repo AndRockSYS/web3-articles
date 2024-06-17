a2f10a0f0bbc5e20886c972a5331cd1703d25ea3
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-17 18:20:58 +0300

    added data and blocks to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,93 @@
+import ArticlePreview from '@/components/ArticlePreview';
+
+import { ArticleBlock } from 'typings';
+
+import './homepage.css';
+
 export default function Home() {
-    return <main></main>;
+    const articles: ArticleBlock[] = [
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+        {
+            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
+            timestamp: Date.now(),
+
+            name: 'Test article',
+            description: ' Here is my testing description, I wonder how to make that, so read.',
+            mainImage:
+                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
+        },
+    ];
+
+    return (
+        <main className='home'>
+            <aside>The home for web3 publishing</aside>
+            <section>
+                {articles.map((article) => (
+                    <ArticlePreview key={article.timestamp} article={article} />
+                ))}
+            </section>
+        </main>
+    );
 }

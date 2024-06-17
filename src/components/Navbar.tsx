8de0b0f7c9b7bda9c370efaa3393b4a52880e1d8
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-17 18:21:04 +0300

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Mirror Copy</h1>
+            <button>Connect</button>
+        </nav>
+    );
+}

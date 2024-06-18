c8eb0b5a2dfcfcbd0f3cdb5b0eaef3ce44fe5c5f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-18 11:58:30 +0300

    added coverImage

--- src/app/add-article/page.tsx
+++ src/app/add-article/page.tsx
@@ -2,9 +2,13 @@
 
 import Image from 'next/image';
 
+import { useMemo, useState } from 'react';
+
 import './add-article.css';
 
 export default function AddArticle() {
+    const [coverImage, setCoverImage] = useState('');
+
     const cutToLimit = (limit: number, event: React.FormEvent<HTMLParagraphElement>) => {
         const value = event.currentTarget.textContent;
         if (value) event.currentTarget.textContent = value.slice(0, limit);
@@ -18,8 +22,53 @@ export default function AddArticle() {
         { value: 'link', name: 'Link' },
     ];
 
+    const openCloseElements = () => {
+        const list = document.querySelector('main.add-article > div > ul') as HTMLElement;
+        list.style.display = list.style.display.includes('none') ? 'block' : 'none';
+    };
+
+    const handleImageInput = async (image: File) => {
+        const arrayBuffer = await image.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        setCoverImage(`data:image/png;base64,${buffer.toString('base64')}`);
+    };
+
     return (
         <main className='add-article'>
+            {useMemo(
+                () =>
+                    coverImage ? (
+                        <div className='image-container'>
+                            <Image
+                                src={coverImage}
+                                alt='cover'
+                                width={1920}
+                                height={1080}
+                                objectFit='cover'
+                            />
+                        </div>
+                    ) : (
+                        <div id='gray-button' className='cover-input'>
+                            <input
+                                type='file'
+                                accept='image/png, image/jpeg'
+                                onChange={(event) => {
+                                    if (!event.target.files) return;
+                                    handleImageInput(event.target.files[0]);
+                                }}
+                            />
+                            <Image
+                                src={'/icons/cover-image.svg'}
+                                alt='cover-image'
+                                width={32}
+                                height={32}
+                            />
+                            Cover Image
+                        </div>
+                    ),
+                [coverImage]
+            )}
+
             <p
                 custom-placeholder='Give it a title'
                 id='title'
@@ -32,8 +81,10 @@ export default function AddArticle() {
                 contentEditable
                 onInput={(event) => cutToLimit(50, event)}
             ></p>
-            <p custom-placeholder='What do you want to write?' id='article' contentEditable></p>
-            <div>
+            <div className='article'>
+                <p custom-placeholder='What do you want to write?' id='article' contentEditable></p>
+            </div>
+            <div className='tool'>
                 <ul>
                     {elements.map((element) => (
                         <li value={element.value}>
@@ -49,9 +100,9 @@ export default function AddArticle() {
                         </li>
                     ))}
                 </ul>
-                <button>
-                    <Image src={'/icons/add.svg'} alt='add' width={32} height={32}></Image>Add
-                    Element
+                <button id='gray-button' onClick={openCloseElements}>
+                    <Image src={'/icons/add.svg'} alt='add' width={32} height={32} />
+                    Add Element
                 </button>
             </div>
         </main>

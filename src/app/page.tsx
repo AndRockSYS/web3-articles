a1ac6ecf64e65e95cd95865d85cc8e99d8a922b6
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-19 14:22:05 +0300

    implemented the hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,26 @@
+'use client';
+
+import { useMemo } from 'react';
+
 import ArticleCell from '@/components/ArticleCell';
 
-import { Article } from 'typings';
+import useFirebase from '@/hooks/useFirebase';
 
 import './homepage.css';
 
 export default function Home() {
-    const articles: Article[] = [
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
+    const { articles } = useFirebase();
 
-            title: 'The name of this article is very very long idk why',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description:
-                ' Here is my testing description, I wonder how to make that, so read. Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-        {
-            creator: '0x497b83b8cd2ae8b51592fcb6525dca51d43cb0c95601323aba4f2aa44b3dfdc1',
-            timestamp: Date.now(),
-
-            title: 'Test article',
-            description: ' Here is my testing description, I wonder how to make that, so read.',
-            coverImage:
-                'https://images.mirror-media.xyz/publication-images/4zFqsBnSZL1IzFogWcsba.png?height=1440&width=2880',
-            tags: 'DeFi/Gaming/DAO/AI',
-            article: '',
-        },
-    ];
+    const cells = useMemo(() => {
+        if (!articles) return <></>;
+        return Object.values(articles).map((article) => (
+            <ArticleCell key={article.tokenId} article={article} />
+        ));
+    }, [articles]);
 
     return (
         <main className='home'>
-            <section>
-                {articles.map((article) => (
-                    <ArticleCell key={article.timestamp} article={article} />
-                ))}
-            </section>
+            <section>{cells}</section>
         </main>
     );
 }

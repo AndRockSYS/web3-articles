7b7573b1dd4f78d4cd250135222ec99ac19ba9d1
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-19 09:48:21 +0300

    added API to call firebase

---
+++ src/app/api/firebase/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import Firebase from '@/services/Firebase';
+
+import { Article } from 'typings';
+
+interface Body {
+    articleAddress?: string;
+    article?: Article;
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        const body: Body = await req.json();
+
+        const firebase = new Firebase();
+        await firebase.authenticate();
+
+        const data = body.articleAddress
+            ? await firebase.getArtilce(body.articleAddress)
+            : await firebase.getAllArticles();
+
+        return NextResponse.json({ data }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500, statusText: 'Internal Error' });
+    }
+}
+
+export async function PUT(req: NextRequest) {
+    try {
+        const body: Body = await req.json();
+
+        if (!body.article) throw new Error('No article data was provided');
+        if (!body.articleAddress) throw new Error('No article address was provided');
+
+        const firebase = new Firebase();
+        await firebase.authenticate();
+
+        await firebase.addArticle(body.articleAddress, body.article);
+
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500, statusText: 'Internal Error' });
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const body: Body = await req.json();
+
+        if (!body.articleAddress) throw new Error('No article address was provided');
+
+        const firebase = new Firebase();
+        await firebase.authenticate();
+        await firebase.removeArticle(body.articleAddress);
+
+        return NextResponse.json({}, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500, statusText: 'Internal Error' });
+    }
+}

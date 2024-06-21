527ad58f88da6193ca116b45dc617a228816e2e2
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-21 11:38:03 +0300

    added cotnract address

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,8 @@ const nextConfig = {
     },
     env: {
         NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
+        NEXT_PUBLIC_MODULE_ADDRESS:
+            '17b0b02730e279de1929daf21beafeadc164ac5d844699c04e0fafb2b5026084',
     },
 };
 

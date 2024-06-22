4e76270edc9fbf867b3a94558205173613c11117
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-22 22:20:29 +0300

    updated the config

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
         ],
     },
     env: {
-        NEXT_PUBLIC_APP_URL: 'https://web3-articles.vercel.app', //'http://localhost:3000',
+        NEXT_PUBLIC_APP_URL: 'https://web3-articles.vercel.app',
         NEXT_PUBLIC_MODULE_ADDRESS:
-            '17b0b02730e279de1929daf21beafeadc164ac5d844699c04e0fafb2b5026084',
+            '72aba4bdb1e35e08a8335002ff708105aaa0acb2aa461d962c6170aaa5115797',
         NEXT_PUBLIC_OWNER: '5d49fe998ce0f9969d4ccbf67e7496c2a5d5f3826296d56673246f43c817b56a',
     },
 };

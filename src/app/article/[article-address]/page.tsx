958973230dda7599bb6db8ddd9060e24c9100869
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-22 13:02:34 +0300

    added button for owner to delete article

--- src/app/article/[article-address]/page.tsx
+++ src/app/article/[article-address]/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+
+import { useWallet } from '@aptos-labs/wallet-adapter-react';
 
 import useFirebase from '@/hooks/useFirebase';
 import { toAuthorTag, toDateTag, toTag } from '@/utils/tagsConverter';
@@ -10,10 +12,10 @@ import { toAuthorTag, toDateTag, toTag } from '@/utils/tagsConverter';
 import { Article } from 'typings';
 
 import './article.css';
-
 export default function ArticlePage() {
+    const { account } = useWallet();
     const path = usePathname();
-    const { getArticle } = useFirebase();
+    const { getArticle, deleteArticle } = useFirebase();
 
     const [article, setArticle] = useState<Article>();
 
@@ -21,10 +23,22 @@ export default function ArticlePage() {
         getArticle(path.split('/')[2]).then((data) => setArticle(data));
     }, [path]);
 
+    const deleteButton = useMemo(
+        () =>
+            account?.address.includes(process.env.NEXT_PUBLIC_OWNER as string) && article ? (
+                <button id='red-button' onClick={() => deleteArticle(article?.tokenId)}>
+                    Delete
+                </button>
+            ) : (
+                <></>
+            ),
+        [account, article]
+    );
+
     if (!article)
         return (
             <main className='loading'>
-                <h1>Loading the article...</h1>
+                <h1>Loading the article</h1>
             </main>
         );
 
@@ -37,7 +51,10 @@ export default function ArticlePage() {
             <h1>
                 <span>{article.title}</span>
             </h1>
-            <div className='author'>{toAuthorTag(article.creator)}</div>
+            <div className='author'>
+                {toAuthorTag(article.creator)}
+                {deleteButton}
+            </div>
             <Image src={article?.coverImage} alt='cover-image' height={360} width={640}></Image>
             <article dangerouslySetInnerHTML={{ __html: article.article }} />
         </main>

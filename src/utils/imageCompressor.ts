9eece377dd438a15f52264ffec15a29fe4566f4f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-22 13:07:48 +0300

    added error preventing

--- src/utils/imageCompressor.ts
+++ src/utils/imageCompressor.ts
@@ -6,10 +6,13 @@ export default async function imageCompressor(imageInput: File) {
     const bfr = await Image.load(Buffer.from(arrayBuffer));
     const resizedImage = bfr.resize({ width: 640, preserveAspectRatio: true });
 
-    const croppedImage = resizedImage.crop({
-        width: 640,
-        height: 360,
-    });
+    const croppedImage =
+        resizedImage.width > 640 && resizedImage.height > 360
+            ? resizedImage.crop({
+                  width: 640,
+                  height: 360,
+              })
+            : resizedImage;
 
     return `data:image/png;base64,${Buffer.from(croppedImage.toBuffer()).toString('base64')}`;
 }

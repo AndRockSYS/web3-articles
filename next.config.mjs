d6c07b188e2668748da90fd635c9794d7e61ce2f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-22 15:05:30 +0300

    changed for prod

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
         ],
     },
     env: {
-        NEXT_PUBLIC_APP_URL: 'http://localhost:3000', //https://web3-articles.vercel.app',
+        NEXT_PUBLIC_APP_URL: 'https://web3-articles.vercel.app', //'http://localhost:3000',
         NEXT_PUBLIC_MODULE_ADDRESS:
             '17b0b02730e279de1929daf21beafeadc164ac5d844699c04e0fafb2b5026084',
         NEXT_PUBLIC_OWNER: '5d49fe998ce0f9969d4ccbf67e7496c2a5d5f3826296d56673246f43c817b56a',

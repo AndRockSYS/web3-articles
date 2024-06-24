ed080c60b3f6c8986f2c89e9fb489b7ffab90dbd
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-24 11:09:07 +0300

    improved loading

--- src/context/ArticleProvider.tsx
+++ src/context/ArticleProvider.tsx
@@ -7,9 +7,13 @@ import { Article } from 'typings';
 export const ArticleContext = createContext<{
     articles: Article[];
     setArticles: Dispatch<SetStateAction<Article[]>>;
+    banners: string[];
+    setBanners: Dispatch<SetStateAction<string[]>>;
 }>({
     articles: [],
     setArticles: () => {},
+    banners: [],
+    setBanners: () => {},
 });
 
 export function ArticleProvider({
@@ -18,12 +22,15 @@ export function ArticleProvider({
     children: React.ReactNode;
 }>) {
     const [articles, setArticles] = useState<Article[]>([]);
+    const [banners, setBanners] = useState<string[]>([]);
 
     return (
         <ArticleContext.Provider
             value={{
                 articles,
                 setArticles,
+                banners,
+                setBanners,
             }}
         >
             {children}
